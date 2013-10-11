17bedf7a97a1bd7c2c40ffdc56848dc6603b893f
kienle <kienctlm123@gmail.com>
2013-10-11 16:38:07 +0700

    commit first project

---
+++ BaeYongJin/src/com/kienle/famous/people/baeyongjin/helper/Config.java
@@ -0,0 +1,9 @@
+package com.kienle.famous.people.baeyongjin.helper;
+
+import android.os.Environment;
+
+public class Config {
+    public static final String DATA_ZIP_FILE = "data.zip";
+    public static final String APP_FOLDER = Environment.getExternalStorageDirectory().getPath() + "/BaeYongJin";
+    public static final String FOLDER_IMAGE = APP_FOLDER + "/images";
+}
